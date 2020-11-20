e86ad686adb11cbf9278bd026ea8957e94729979
toledompm <toledompm@gmail.com>
2020-11-20 20:53:38 -0300

    removing references to domain

--- course-api/src/app.module.ts
+++ course-api/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UniversityModule } from './domain/university/university.module';
+import { UniversityModule } from './university/university.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), UniversityModule],
