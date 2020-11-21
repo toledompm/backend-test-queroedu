70042c3990555dedae462dbe23abe76e4729fbcd
toledompm <toledompm@gmail.com>
2020-11-21 18:30:19 -0300

    More specs :D

--- course-api/src/university/university.services.spec.ts
+++ course-api/src/university/university.services.spec.ts
@@ -6,11 +6,7 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { UniversityCreateDto } from './interface/university-create.dto';
 import { UniversityRepository } from './university.repository';
 
-const {
-  validUniversity,
-  validUniversityDto,
-  invalidUniversityDto,
-} = UniversityMocks;
+const { validUniversity, validUniversityDto } = UniversityMocks;
 
 describe('UniversityService', () => {
   let universityService: UniversityService;
@@ -42,10 +38,56 @@ describe('UniversityService', () => {
       },
     );
 
-    return universityService
-      .createUniversity(validUniversityDto)
-      .then((data) => {
-        expect(data).toEqual(validUniversity);
-      });
+    await expect(
+      universityService.createUniversity(validUniversityDto),
+    ).resolves.toEqual(validUniversity);
+  });
+
+  it('should return an university when searching by name', async () => {
+    const validUniversityName = 'Existing university';
+
+    jest.spyOn(universityRepositoryMock, 'getByName').mockImplementation(
+      (_universityName: string): Promise<University> => {
+        expect(_universityName).toEqual(validUniversityName);
+
+        return Promise.resolve(validUniversity);
+      },
+    );
+
+    await expect(
+      universityService.getByName(validUniversityName),
+    ).resolves.toEqual(validUniversity);
+  });
+
+  it('should throw not_found exception when searching for non-existing university', async () => {
+    const invalidUniversityName = 'Fake Name';
+
+    jest.spyOn(universityRepositoryMock, 'getByName').mockImplementation(
+      (_universityName: string): Promise<University> => {
+        expect(_universityName).toEqual(invalidUniversityName);
+
+        return Promise.resolve(undefined);
+      },
+    );
+
+    await expect(
+      universityService.getByName(invalidUniversityName),
+    ).rejects.toThrow('Not Found');
+  });
+
+  it('should throw not_found when deleting non-existing university', async () => {
+    const nonExistingId = -1;
+
+    jest.spyOn(universityRepositoryMock, 'getById').mockImplementation(
+      (_id: number): Promise<University> => {
+        expect(_id).toEqual(nonExistingId);
+
+        return Promise.resolve(undefined);
+      },
+    );
+
+    await expect(universityService.deleteById(nonExistingId)).rejects.toThrow(
+      'Not Found',
+    );
   });
 });
