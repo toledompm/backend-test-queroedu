f097ab13196d06c27266680d9b81ab74425a17f8
toledompm <toledompm@gmail.com>
2020-11-20 21:04:21 -0300

    Add exception filter

--- course-api/src/main.ts
+++ course-api/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './common/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
   await app.listen(3000);
 }
 bootstrap();
