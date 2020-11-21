05d5deaae63539d1faec6fe9dde111cd52665f18
toledompm <toledompm@gmail.com>
2020-11-21 01:44:24 -0300

    Implementing tests

---
+++ course-api/src/university/university.services.spec.ts
@@ -0,0 +1,51 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UniversityMocks } from '../__mocks__/university';
+import { UniversityService } from './university.service';
+import { University } from './university.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UniversityCreateDto } from './interface/university-create.dto';
+import { UniversityRepository } from './university.repository';
+
+const {
+  validUniversity,
+  validUniversityDto,
+  invalidUniversityDto,
+} = UniversityMocks;
+
+describe('UniversityService', () => {
+  let universityService: UniversityService;
+  let universityRepositoryMock: UniversityRepository;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UniversityService,
+        {
+          provide: getRepositoryToken(University),
+          useClass: UniversityRepository,
+        },
+      ],
+    }).compile();
+
+    universityService = module.get<UniversityService>(UniversityService);
+    universityRepositoryMock = module.get(
+      getRepositoryToken(University),
+    ) as UniversityRepository;
+  });
+
+  it('should create a university', async () => {
+    jest.spyOn(universityRepositoryMock, 'createUniversity').mockImplementation(
+      (_universityDto: UniversityCreateDto): Promise<University> => {
+        expect(_universityDto).toEqual(validUniversityDto);
+
+        return Promise.resolve(validUniversity);
+      },
+    );
+
+    return universityService
+      .createUniversity(validUniversityDto)
+      .then((data) => {
+        expect(data).toEqual(validUniversity);
+      });
+  });
+});
